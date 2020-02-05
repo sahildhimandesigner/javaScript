450e990c78dd364546f490180148908fa3291a58
sahildhimandesigner <sahil@ucreate.co.in>
2020-02-05 18:07:00 +0530

    how to update the table data in firebase with javascript

--- my-schedule/welcome.js
+++ my-schedule/welcome.js
@@ -38,9 +38,9 @@
    }
 
    // Created the function for updated the user table
-   const editDetails = (e) => {
-     alert(e.id, "edit the user details")
-   }
+  //  const editDetails = (e) => {
+  //    alert(e.id, "edit the user details")
+  //  }
 
    // I have created the function fo delete the user
    const deleteDetails = () => {
@@ -58,24 +58,29 @@
     document.getElementById("ex-table").innerHTML = '';
     
     console.log('step 1', userData);
-    database.ref(`message/${userData.uid}`).once('value', function(snapshot){
+
+    var getUserId = userData.uid;
+
+    database.ref(`message/${getUserId}`).once('value', function(snapshot){
       console.log('step 2',snapshot.exists());
-        loggedInUserDetails(snapshot, flag)
+        loggedInUserDetails(snapshot, flag, getUserId)
       });
     }
    
    // pass the two parameter to call this function and use in GETALLUSERDATA function   
-   const loggedInUserDetails = (snapshot, flag) => {
+   const loggedInUserDetails = (snapshot, flag, getUserId) => {
     if(snapshot.exists()){
       document.getElementById("data_loading").style.display = "none";
       var content = '';             
       snapshot.forEach(function(data){
-          var val = data.val();                
+          var val = data.val();   
+          var userDataKey = data.key; 
+          
           if(flag == 'list') {
             content = showtUserTableData(val,content)
           }
           else{
-            content = eidtUserTableData(val, content);
+            content = eidtUserTableData(val, content, userDataKey, getUserId);
           }
           document.getElementById("ex-table").innerHTML += content;             
       });
@@ -100,14 +105,14 @@
  }
 
  // Created the seperate fucntion to show the user details with prefield input value and pass the parameter as value to function so the we function understading
-   const eidtUserTableData = (val, content) => {
+   const eidtUserTableData = (val, content, userDataKey, getUserId) => {
       content +='<tr>'
       content +='<td width="20%">'+ '<input type="text" name="name" value="'+ val.name + '" size="20" />' +'</td>';
       content +='<td width="20%">'+ '<input type="text" name="lastName" value="'+ val.email + '" size="20" />' +'</td>';
       content +='<td width="20%">'+ '<input type="email" name="email" value="'+ val.phone + '" size="20" />' +'</td>';
       content +='<td width="20%">'+ '<input type="phone" name="" value="'+ val.lastName + '" size="20" />' +'</td>';
       content +='<td width="20%">'+ '<textarea name="message"> '+ val.message +'</textarea>' +'</td>';
-      content += '<td width="20%">' + '<button>Update</button>' +'</td>';
+      content += '<td width="20%">' + '<button onclick="updateUserTable(\''+userDataKey+  '\',\'' + getUserId + ' \')">Update</button>' +'</td>';
       content +='</tr>'
       //we have to return the content becuse we have to use this in other function
       return content;
@@ -116,4 +121,15 @@
    //Pass the list as value to function so that we can check that is flas is equal to list then call SHOWUSERTABLEDATA otherwise editUserTableData
    var usDa = window.onload = getAllUserData('list');
 
-   
+   const updateUserTable = (id, getUserId) => {
+    var database = firebase.database();
+      
+    //get the input value on click button bassed on input attribute
+     var upUserName = document.getElementsByName('name')[0].value;
+     alert(upUserName)
+     var uptb = database.ref(`message/${getUserId}`).child(id).update({name: upUserName})
+     
+     console.log(uptb, "uptbuptbuptbuptbuptbuptb")
+
+     getAllUserData('list');
+   }
