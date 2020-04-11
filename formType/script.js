6c189cfc96a75be095f042f1ab39dd943da369ed
sahildhimandesigner <sahil@ucreate.co.in>
2020-04-11 15:50:20 +0530

    create the d functionality to display the input value

--- formType/script.js
+++ formType/script.js
@@ -1,20 +1,24 @@
 init();
 
+//BIND THE FUNCTION, ID AND CLASSES
 document.getElementById("addInput").addEventListener('click', openModel);
 document.querySelector('.closeModal').addEventListener('click', closeModal)
 document.querySelector("#add").addEventListener('click', selectInputType);
 document.querySelector('.form-container').style.display = 'none';
 
+//CREATED THE FUNCTION FOR CLOSE THE MODAL
 function closeModal(){
     document.querySelector(".modal").style.display = 'none';
     document.querySelector('body').classList.remove('modal-overlay');
 }
 
+//CREATED THE FUNCTION FOR OPEN MODAL
 function openModel(){
     document.querySelector(".modal").style.display = 'block';
     document.querySelector('body').classList.add('modal-overlay');
 }
 
+//CREATED THE FUNCTION FOR SELECT THE FORM TYPE, NAME, ID AND VALUE
 function selectInputType() {
     inputType = document.querySelector("#selectInputType").value;
     inpuName = document.querySelector('#selectInputName').value;
@@ -25,17 +29,20 @@ function selectInputType() {
     document.querySelector('.form-container').style.display = 'block';
     document.querySelector(".add-input-btn").style.display = 'none';
     closeModal()
+    document.querySelector(".submit-btn").innerHTML = `<input class="sub-btn" type="submit" value="submit" />`
+    document.querySelector(".sub-btn").addEventListener('click', submitForm);
 }
 
 closeModal();
 
-function showUserValue() {
-    // var showInputValue = document.getElementById(``).value;
-    
+//DISPLAY THE INPUT VALUE ON FRONTEND
+function submitForm() {
+    inputValue = document.querySelector(`#${inputId}`).value;
+    document.querySelector(".show_input_value").textContent = inputValue;
 }
 
+//CREATED THE FUNCTION FOR DECALARE THE VARRIABLE
 function init() {
-    var inputType, lableName, inpuName, inputId;
+    var inputType, lableName, inpuName, inputId, storeId;
 }
-showUserValue()
 
