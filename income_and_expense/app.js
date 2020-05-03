7fbe8ff3c20a617670ee82e7fdac2284b3e974fb
sahildhimandesigner <sahil@ucreate.co.in>
2020-05-03 17:33:53 +0530

    created the methode and return the object to get input values

--- income_and_expense/app.js
+++ income_and_expense/app.js
@@ -1,25 +1,77 @@
-const addValue = () => {    
-    var descInput = document.querySelector('.add__description').value;
-    var ammountInput = document.querySelector('.add__value').value;        
-    income(descInput, ammountInput);
-    expense(descInput, ammountInput);
-}
+//BUDGET CONTROLLER
+var budegtController = (function(){
+    
+    //Some code
 
-const getSelectValue = () => {
-    alert('d')
-}
+})();
 
-var d = document.querySelector('.add__type').addEventListener('onChange', getSelectValue);
+// UI CONTROLLER
 
-const income = (descInput, ammountInput) => {
-    document.querySelector('.item__description').innerHTML = descInput;
-    document.querySelector('.item__value').innerHTML = ammountInput;
-}
+var UIController = (function() {
 
-const expense = (descInput, ammountInput) => {
-    document.querySelector('.item__description').innerHTML = descInput;
-    document.querySelector('.item__value').innerHTML = ammountInput;
-}
+    //DESCRIPTION ABOUT THE CONTROLLER--------------------------------
+    //HERE IN UI CONTRLLER WE ARE GOING TO WRITE A PUBLIC METHODE OR FUNCTION.
+    //SO IT CAN NOT BE PRIVATE FUNCTION.
+    //THIS IS FUNCTION WHICH YOU HAVE TO USE IN OTHER CONTROLLER.
+    //SO THIS FUNCTION EXECUTE IMMEDIATELY AND THE OBJECT WHICH WE RETURN THAT ASSIGN THIS CONTRLLER.
+    //ALL THE VARRIABLE AND FUNCTION WHICH WE DEFINE WILL STAY IN CLOUSE AFTER THIS FUNCTOIN RETURN.
+    //THIS OJECT WIHCH WE ARE GOING TO REUTN THAT HAVE ACCEES THE PRIVATE FUNCTION AND VARRIABLE.
+
+    //SOME CODE
+
+    return {
+        //Here we have created the methode and return a object becuse we can access it publically.
+        getInput: function() {
+            return {
+
+                //So rather than defining multple var we create a object.
+                //this methode return all these three input which have this controller.
+                type: document.querySelector('.add__type').value,
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value,
+
+                //OTHER WAY IS
+
+                //var type = document.querySelector('.add__type').value,
+                //var description = document.querySelector('.add__description').value,
+                //var value = document.querySelector('.add__value').value,
+
+            }
+        }
+    }
+
+})();
 
 
-document.querySelector('.add__btn').addEventListener('click', addValue)
+//GLOBAL APP CONTROLLER
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    //For reusability we have created the function.
+    var ctrlAddItem = function() {
+
+        // 1. Get the filed input data
+        const input = UICtrl.getInput();
+        console.log(input);
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI 
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+    }
+
+    //Bind the click methode with add button and call a function
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+    //Created the event on press ENTER key
+    document.addEventListener('keypress', function(event) {
+        if(event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    })
+
+})(budegtController, UIController);
